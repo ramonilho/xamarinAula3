14f8e63e8f6b6d2764838a4e5924a7a35bddcde6
Ramon Honorio <ramonaqh@gmail.com>
2017-11-01 22:48:12 -0200

    initial commit

---
+++ XF.Recursos/XF.Recursos/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Recursos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSimples_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.SimplesPage());
+        }
+
+        private async void btnGlobal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.GeralView());
+        }
+
+        private async void btnDinamico_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.DinamicoView());
+        }
+
+        private async void btnTriggers_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.TriggersView());
+        }
+
+        private async void btnTemplate_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Theme.ThemeView());
+        }
+
+        private async void btnHome_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PassParameter.HomeView(DateTime.Now.ToString("u")));
+        }
+
+        private async void btnMC_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PassParameter.MCHomeView());
+        }
+
+        private async void btnMestre_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Menu.MainPage());
+        }
+    }
+}
